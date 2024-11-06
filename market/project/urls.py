2fb2f2dc61ec9f345793d2d4fef62826908438c7
rustem <ragamyradow22@gmail.com>
2024-11-06 16:49:50 +0500

    second

--- market/project/urls.py
+++ market/project/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('product',views.home,name='product'),
 
 ]
--- market/project/views.py
+++ market/project/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def home(request):
     return render(request,'index.html')
 
+def product(request):
+    return render(request,'product.html')
+
 # Create your views here.
