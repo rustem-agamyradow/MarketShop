379e280cc7de3072ea1b516de09528166b4a6a27
Guwanch119 <guwanchgylychlyyev@gmail.com>
2024-12-11 17:30:52 +0500

    login

--- market/project/urls.py
+++ market/project/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('product',views.product,name='product'),
-    path('login',views.login,name='login'),
-    path('register',views.register,name='register'),
-    path('electoronics',views.electronics,name='electronics'),    path('register',views.register,name='register'),
+    path('logout/',views.logoutView,name='logout'),
+    path('login/',views.loginView,name='login'),
+    path('register/',views.registerView,name='register'),   
     path('electronics',views.electronics,name='electronics'),
     path('about',views.about,name='about'),
     path('checkout',views.checkout,name='checkout'),
--- market/project/views.py
+++ market/project/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     return render(request,'index.html')
@@ -7,20 +9,57 @@ def home(request):
 def product(request):
     return render(request,'product.html')
 
-def login(request):
-    return render(request,'login.html')
+def logoutView(request):
+    logout(request)
+    return redirect('home')
 
-def register(request):
-    return render(request,'register.html')
+def loginView(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request,username = username,password = password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    return redirect('home')
+                
+                else:
+                    messages.info(request,'Disabled Account')
+            
+            else:
+                messages.info(request,'Check your username and password')
+
+    else:
+        form = LoginForm()
+
+    return render(request,'login.html',{'form':form})
+
+
+
+
+
+
+def registerView(request):
+    
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Account has been created, You can login')
+            return redirect('login')
+        
+    else:
+        form = RegisterForm()
+        
+
+    return render(request,'register.html',{'form':form})
 
 def electronics(request):
     return render(request,'electronics.html')
 
-def register(request):
-    return render(request,'register.html')
-
-def electronics(request):
-    return render(request,'electronics.html')
 
 
 def about(request):
